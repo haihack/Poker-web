29ec50e42dd9d0c4667d04e3839dd7dd4e3a146e
haihack <chuhoanghai0901@gmail.com>
2020-11-27 02:06:55 +0900

    - Improving code base on vlai's feedback

--- spec/view/index_spec.rb
+++ spec/view/index_spec.rb
@@ -4,93 +4,95 @@ require 'rails_helper'
 RSpec.describe "index.html", type: :feature do
   include Constants
 
+  let(:url) { "/welcome/index" }
+
   describe 'ok' do
     it 'should return ROYAL_FLUSH hand' do
-      visit "/welcome/index"
+      visit url
       fill_in 'my_data', with: 'H1 H13 H12 H11 H10'
       # find('input[name="submit"]').click
       submit_form
 
-      expect(page).to have_content $handTypes[$handTypes_royal_flush]
+      expect(page).to have_content HAND_TYPES[HANDTYPES_ROYAL_FLUSH]
     end
 
     it 'should return STRAIGHT_FLUSH hand' do
-      visit "/welcome/index"
+      visit url
       fill_in 'my_data', with: 'H1 H2 H3 H4 H5'
       submit_form
 
-      expect(page).to have_content $handTypes[$handTypes_straight_flush]
+      expect(page).to have_content HAND_TYPES[HANDTYPES_STRAIGHT_FLUSH]
     end
 
     it 'should return FOUR_OF_A_KIND hand' do
-      visit "/welcome/index"
+      visit url
       fill_in 'my_data', with: 'H1 H7 D7 C7 S7'
       submit_form
 
-      expect(page).to have_content $handTypes[$handTypes_4_of_a_kind]
+      expect(page).to have_content HAND_TYPES[HANDTYPES_4_OF_A_KIND]
     end
 
     it 'should return FULL_HOUSE hand' do
-      visit "/welcome/index"
+      visit url
       fill_in 'my_data', with: 'H1 S1 D1 H5 D5'
       submit_form
 
-      expect(page).to have_content $handTypes[$handTypes_full_house]
+      expect(page).to have_content HAND_TYPES[HANDTYPES_FULL_HOUSE]
     end
 
     it 'should return FLUSH hand' do
-      visit "/welcome/index"
+      visit url
       fill_in 'my_data', with: 'H1 H13 H5 H6 H10'
       submit_form
 
-      expect(page).to have_content $handTypes[$handTypes_flush]
+      expect(page).to have_content HAND_TYPES[HANDTYPES_FLUSH]
     end
 
     it 'should return STRAIGHT hand' do
-      visit "/welcome/index"
+      visit url
       fill_in 'my_data', with: 'H1 C2 D3 H4 H5'
       submit_form
 
-      expect(page).to have_content $handTypes[$handTypes_straight]
+      expect(page).to have_content HAND_TYPES[HANDTYPES_STRAIGHT]
     end
 
     it 'should return THREE_OF_A_KIND hand' do
-      visit "/welcome/index"
+      visit url
       fill_in 'my_data', with: 'H1 D1 C1 H11 D10'
       submit_form
 
-      expect(page).to have_content $handTypes[$handTypes_3_of_a_kind]
+      expect(page).to have_content HAND_TYPES[HANDTYPES_3_OF_A_KIND]
     end
 
     it 'should return TWO_PAIRS hand' do
-      visit "/welcome/index"
+      visit url
       fill_in 'my_data', with: 'H1 D1 H12 D12 C5'
       submit_form
 
-      expect(page).to have_content $handTypes[$handTypes_2_pairs]
+      expect(page).to have_content HAND_TYPES[HANDTYPES_2_PAIRS]
     end
 
     it 'should return ONE_PAIR hand' do
-      visit "/welcome/index"
+      visit url
       fill_in 'my_data', with: 'H1 D1 H5 S11 H10'
       submit_form
 
-      expect(page).to have_content $handTypes[$handTypes_1_pair]
+      expect(page).to have_content HAND_TYPES[HANDTYPES_1_PAIR]
     end
 
     it 'should return HIGH_CARD hand' do
-      visit "/welcome/index"
+      visit url
       fill_in 'my_data', with: 'H1 D13 C4 S8 H10'
       submit_form
 
-      expect(page).to have_content $handTypes[$handTypes_high_card]
+      expect(page).to have_content HAND_TYPES[HANDTYPES_HIGH_CARD]
     end
 
   end
 
   describe 'failure' do
     it 'empty hand' do
-      visit "/welcome/index"
+      visit url
       fill_in 'my_data', with: ''
       submit_form
 
@@ -98,7 +100,7 @@ RSpec.describe "index.html", type: :feature do
     end
 
     it 'over 5 cards in a hand' do
-      visit "/welcome/index"
+      visit url
       fill_in 'my_data', with: 'H1 H2 H3 H4 H5 H6 H7'
       submit_form
 
@@ -106,7 +108,7 @@ RSpec.describe "index.html", type: :feature do
     end
 
     it 'under 5 cards in a hand' do
-      visit "/welcome/index"
+      visit url
       fill_in 'my_data', with: 'H1 H2 H3'
       submit_form
 
@@ -114,7 +116,7 @@ RSpec.describe "index.html", type: :feature do
     end
 
     it 'multiple space between cards' do
-      visit "/welcome/index"
+      visit url
       fill_in 'my_data', with: 'H1  H2  H3 H4 H5'
       submit_form
 
@@ -122,7 +124,7 @@ RSpec.describe "index.html", type: :feature do
     end
 
     it 'invalid card name` s format' do
-      visit "/welcome/index"
+      visit url
       fill_in 'my_data', with: 'H111 H2 H3 H4 H5'
       submit_form
 
@@ -130,7 +132,7 @@ RSpec.describe "index.html", type: :feature do
     end
 
     it 'duplicated cards' do
-      visit "/welcome/index"
+      visit url
       fill_in 'my_data', with: 'H1 H1 H3 H4 H5'
       submit_form
 
